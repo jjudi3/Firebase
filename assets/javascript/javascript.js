3ec666daa16cca9266222b482cac8da22c168dd0
Judi Perez <j_vperez29@bellsouth.net>
2018-01-20 06:58:50 -0500

    first commit

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,150 @@
+// Initialize Firebase
+
+
+    // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCIsNXs6qMdcMRhOnR5vO1p1QAAWzzJbgI",
+    authDomain: "bootcamp-example-23c60.firebaseapp.com",
+    databaseURL: "https://bootcamp-example-23c60.firebaseio.com",
+    projectId: "bootcamp-example-23c60",
+    storageBucket: "bootcamp-example-23c60.appspot.com",
+    messagingSenderId: "869660441150"
+  };
+  firebase.initializeApp(config);
+
+
+var database = firebase.database();
+
+// Initial Values
+var trainName = "";
+var destination = "";
+var firstTrainTime = "";
+var frequency = 0;
+var currentTime = moment();
+var index = 0;
+var trainIDs = [];
+
+// Show current time
+var datetime = null,
+date = null;
+
+var update = function () {
+  date = moment(new Date())
+  datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+};
+
+$(document).ready(function(){
+  datetime = $('#current-status')
+  update();
+  setInterval(update, 1000);
+});
+
+
+// Capture Button Click
+$("#add-train").on("click", function() {
+
+  // Grabbed values from text boxes
+  trainName = $("#train-name").val().trim();
+  destination = $("#destination").val().trim();
+  firstTrainTime = $("#train-time").val().trim();
+  frequency = $("#frequency").val().trim();
+  
+  
+  var firstTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
+ 
+
+  // Difference between the times
+  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+ 
+
+  var tRemainder = diffTime % frequency;
+  
+
+  
+  var minutesAway = frequency - tRemainder;
+  
+
+  // Next Train
+  var nextTrain = moment().add(minutesAway, "minutes");
+  
+
+  // Arrival time
+  var nextArrival = moment(nextTrain).format("hh:mm a");
+
+  var nextArrivalUpdate = function() {
+    date = moment(new Date())
+    datetime.html(date.format('hh:mm a'));
+  }
+
+  // Code for handling the push
+  database.ref().push({
+    trainName: trainName,
+    destination: destination,
+    firstTrainTime: firstTrainTime,
+    frequency: frequency,
+    minutesAway: minutesAway,
+    nextArrival: nextArrival,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
+  });
+  
+  alert("Form submitted!");
+
+  // Empty text input
+  $("#train-name").val("");
+  $("#destination").val("");
+  $("#train-time").val("");
+  $("#frequency").val("");
+  
+  // Don't refresh the page!
+  return false; 
+});
+
+
+
+// Firebase watcher + initial loader HINT: This code behaves similarly to .on("child_added")
+// This will only show the 25 latest entries
+  database.ref().orderByChild("dateAdded").limitToLast(25).on("child_added", function(snapshot) {
+
+
+    console.log("Train name: " + snapshot.val().trainName);
+    console.log("Destination: " + snapshot.val().destination);
+    console.log("First train: " + snapshot.val().firstTrainTime);
+    console.log("Frequency: " + snapshot.val().frequency);
+    console.log("Next train: " + snapshot.val().nextArrival);
+    console.log("Minutes away: " + snapshot.val().minutesAway);
+    console.log("==============================");
+
+
+  // Change the HTML to reflect
+  $("#new-train").append("<tr><td>" + snapshot.val().trainName + "</td>" +
+    "<td>" + snapshot.val().destination + "</td>" + 
+    "<td>" + "Every " + snapshot.val().frequency + " mins" + "</td>" + 
+    "<td>" + snapshot.val().nextArrival + "</td>" +
+    "<td>" + snapshot.val().minutesAway + " mins until arrival" + "</td>" +
+   // "<td><button class='delete btn btn-default btn-sm' data-index='" + index + "'><span class='glyphicon glyphicon-trash'></span></button> " + 
+   // "<button type='button' class='btn btn-default btn-sm'><span class='glyphicon glyphicon-edit'></span></button>" +
+    "</td></tr>");
+
+  index++;
+
+  //  errors
+  }, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+  });
+
+  //Gets the train IDs in an Array
+  database.ref().once('value', function(dataSnapshot){ 
+    var trainIndex = 0;
+
+      dataSnapshot.forEach(
+          function(childSnapshot) {
+              trainIDs[trainIndex++] = childSnapshot.key();
+          }
+      );
+  });
+
+  console.log(trainIDs);
+
+
+
+
